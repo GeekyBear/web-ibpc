86ba8367926c3640ac1dfa91e0c4cb22c8cb907a
Sanchez <ezequiel.sanchez@accenture.com>
2023-11-10 19:13:07 -0300

    background_desplegable

--- src/components/Pray/Pray.js
+++ src/components/Pray/Pray.js
@@ -8,12 +8,15 @@ export default function Pray({ pray, showDetail, mobile }) {
       <li
         id={pray.id}
         onClick={(e) => (mobile ? setShow(!show) : showDetail(e))}
-        className="list-group-item list-group-item-action"
+        className="list-group-item list-group-item-action p-3"
       >
         {pray.data.title}
       </li>
       {mobile && show && (
-        <li className="list-group-item list-group-item-action">
+        <li
+          className="list-group-item list-group-item-action text-start p-4"
+          style={{ backgroundColor: "#f5f5f5" }}
+        >
           {pray.data.description
             ? pray.data.description
             : "No hay una descripcion"}
