45add97a8408054bef43ebf23c1eb9be603e2d1e
Sanchez <ezequiel.sanchez@accenture.com>
2023-11-11 10:31:56 -0300

    Updated color

--- src/components/Pray/Pray.js
+++ src/components/Pray/Pray.js
@@ -4,24 +4,28 @@ export default function Pray({ pray, showDetail, mobile }) {
   const [show, setShow] = useState(false);
 
   return (
-    <>
-      <li
+    <div className="border border-secondary">
+      <div
         id={pray.id}
         onClick={(e) => (mobile ? setShow(!show) : showDetail(e))}
-        className="list-group-item list-group-item-action p-3"
+        className={show ? "bg-body-secondary fw-bold p-3 " : "fw-bold p-3"}
+        // className="list-group-item list-group-item-action p-3"
       >
         {pray.data.title}
-      </li>
+      </div>
       {mobile && show && (
-        <li
-          className="list-group-item list-group-item-action text-start p-4"
-          style={{ backgroundColor: "#f5f5f5" }}
+        <div
+          // className="list-group-item list-group-item-action text-start p-4"
+          className={
+            show ? "bg-body-secondary  border text-start p-4" : "bg-white p-4"
+          }
+          // style={{ backgroundColor: "#f5f5f5" }}
         >
           {pray.data.description
-            ? pray.data.description
+            ? "Descripción: " + pray.data.description
             : "No hay una descripcion"}
-        </li>
+        </div>
       )}
-    </>
+    </div>
   );
 }
